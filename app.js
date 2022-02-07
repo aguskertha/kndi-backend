3acf106ab0abcbb7beb981cdb61974999d840571
aguskertha <baguskertha@gmail.com>
2022-02-07 15:01:54 +0700

    fix connect to mongodb local

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -15,7 +15,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const database = process.env.MONGO_URI;
 const port = process.env.db_port || 27017;
-const databaseLocal = 'mongodb://localhost:27017/kndiDB' || `mongodb://${process.env.db_service_name}:${port}/${process.env.db_name}`;
+const databaseLocal = `mongodb://${process.env.db_service_name}:${port}/${process.env.db_name}`;
 
 mongoose.connect(databaseLocal, {
     useNewUrlParser: true,
