3c5d800d270e6c3b9a6dfb5d2ca0e1c05b9c9465
aguskertha <baguskertha@gmail.com>
2022-02-24 08:05:56 +0700

    done swagger docs api news

--- src/newspost/route.js
+++ src/newspost/route.js
@@ -21,17 +21,17 @@ const {
     updateNewsPostByIDValidation
 } = require('./validation');
 
-router.post('/', createNewsPostValidation, createNewsPost);
+router.post('/', authenticate, createNewsPostValidation, createNewsPost);
 router.get('/', getNewsPosts);
-router.get('/latest', getLatestNewsPosts);
-router.delete('/', deleteNewsPosts);
+router.get('/latest',  getLatestNewsPosts);
+router.delete('/', authenticate, deleteNewsPosts);
 router.get('/:slug', getNewsPostBySlug);
-router.delete('/:newspostID', deleteNewsPostByID);
-router.post('/update', updateNewsPostByIDValidation, updateNewsPostByID);
-router.post('/publish', changeNewsPostToPublish);
-router.post('/draft', changeNewsPostToDraft);
-router.post('/uploadthumbnail', singleUploadFile, createThumbnail);
-router.post('/uploadimages', multiUploadFile, createImages);
+router.delete('/:newspostID', authenticate, deleteNewsPostByID);
+router.post('/update', authenticate, updateNewsPostByIDValidation, updateNewsPostByID);
+router.post('/publish', authenticate, changeNewsPostToPublish);
+router.post('/draft', authenticate, changeNewsPostToDraft);
+router.post('/uploadthumbnail', authenticate, singleUploadFile, createThumbnail);
+router.post('/uploadimages', authenticate, multiUploadFile, createImages);
 
 module.exports = router;
 
@@ -39,7 +39,7 @@ module.exports = router;
 /**
  * @swagger
  * tags:
- *  name: Contact
+ *  name: NewsPost
  */
 
 /**
@@ -85,6 +85,8 @@ module.exports = router;
  *              application/json:
  *                  schema:
  *                      $ref: '#/components/schemas/NewsPost'
+ *      security:
+ *        - bearerAuth: []
  *      responses:
  *          200:
  *              description: The News successfully created
@@ -99,11 +101,248 @@ module.exports = router;
  *  get:
  *      summary: Get All News
  *      tags: [NewsPost]
+ *      responses:
+ *          200:
+ *              description: Successfully get all News
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/{slug}:
+ *  get:
+ *      summary: Get News By Slug
+ *      tags: [NewsPost]
+ *      parameters:
+ *          -   in: path
+ *              name: slug
+ *              schema:
+ *                  type: string
+ *              required: true
+ *      responses:
+ *          200:
+ *              description: The News successfully found!
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/latest:
+ *  get:
+ *      summary: Get Latest News
+ *      tags: [NewsPost]
+ *      responses:
+ *          200:
+ *              description: Successfully get latest News
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/update:
+ *  post:
+ *      summary: Update News By ID
+ *      tags: [NewsPost]
+ *      security:
+ *        - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          _id:
+ *                              type: string
+ *                          thumbnailURL:
+ *                              type: string
+ *                          contents:
+ *                              type: array
+ *                              items:
+ *                                  type: object
+ *                                  properties:
+ *                                      title:
+ *                                          type: String
+ *                                      languageCode:
+ *                                          type: String
+ *                                      data:
+ *                                          type: String
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          _id:
+ *                              type: string
+ *                          thumbnailURL:
+ *                              type: string
+ *                          contents:
+ *                              type: array
+ *                              items:
+ *                                  type: object
+ *                                  properties:
+ *                                      title:
+ *                                          type: String
+ *                                      languageCode:
+ *                                          type: String
+ *                                      data:
+ *                                          type: String
+ *      responses:
+ *          200:
+ *              description: The News successfully updated
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/publish:
+ *  post:
+ *      summary: Change News to Publish
+ *      tags: [NewsPost]
+ *      security:
+ *        - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          newspostID:
+ *                              type: string
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          newspostID:
+ *                              type: string
+ *      responses:
+ *          200:
+ *              description: The News successfully published
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/draft:
+ *  post:
+ *      summary: Change News to Draft
+ *      tags: [NewsPost]
+ *      security:
+ *        - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          newspostID:
+ *                              type: string
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          newspostID:
+ *                              type: string
+ *      responses:
+ *          200:
+ *              description: The News successfully drafted
+ *          400:
+ *              description: Something wrong!
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/newspost/uploadthumbnail:
+ *  post:
+ *      summary: Upload Thumbnail file
+ *      tags: [NewsPost]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          file:
+ *                              type: string
+ *                              format: binary
  *      security:
  *        - bearerAuth: []
  *      responses:
  *          200:
- *              description: Successfully get all News
+ *              description: The Thumbnail Image successfully uploaded
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/uploadimages:
+ *  post:
+ *      summary: Upload Multiple Images file
+ *      tags: [NewsPost]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          files:
+ *                              type: array
+ *                              items:
+ *                                  type: string
+ *                                  format: binary
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: The Images successfully uploaded
+ *          400:
+ *              description: Something wrong!
+ */
+
+
+/**
+ * @swagger
+ * /api/v1/newspost/{newspostID}:
+ *  delete:
+ *      summary: Delete News By ID
+ *      tags: [NewsPost]
+ *      parameters:
+ *          -   in: path
+ *              name: newspostID
+ *              schema:
+ *                  type: string
+ *              required: true
+ *              description: String with length 24 char of the news to get
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: The News successfully deleted!
+ *          400:
+ *              description: Something wrong!
+ */
+
+/**
+ * @swagger
+ * /api/v1/newspost/:
+ *  delete:
+ *      summary: Delete All News
+ *      tags: [NewsPost]
+ *      security:
+ *        - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: The News successfully deleted!
  *          400:
  *              description: Something wrong!
  */
